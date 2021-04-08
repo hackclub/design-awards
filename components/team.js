30788e01e8400188dd774339fe7c9d710921d4c2
eilla1 <exu6056@gmail.com>
2021-04-08 00:48:44 -0700

    fix pfp size on mobile

--- components/team.js
+++ components/team.js
@@ -2,11 +2,7 @@ export default function Team({ src, name, role, pronouns }) {
   return (
     <div className="flex flex-row space-x-3 items-center">
       <div>
-        <img
-          src={src}
-          alt={name}
-          className="rounded-full h-16 w-16 md:w-24 md:h-24"
-        />
+        <img src={src} alt={name} className="rounded-full w-24 h-24" />
       </div>
       <div>
         <p className="uppercase text-xl text-white font-ibm lg:text-2xl">
