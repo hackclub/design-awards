d3b511a3cb05d48826f0086e62a445e78caeee1a
Ella <72365100+eilla1@users.noreply.github.com>
2021-04-08 00:49:52 -0700

    feat: add 3D icons (#11)
    * add highlight color
    
    * add styles for responsiveness and random floaty icons
    
    * fix pfp size on mobile

--- components/award.js
+++ components/award.js
@@ -2,8 +2,8 @@ import { BiTrophy } from "react-icons/bi";
 
 export default function Award({ award }) {
   return (
-    <div className="text-white flex flex-row space-x-4 font-dm text-2xl">
-      <BiTrophy size={30} color="white" />
+    <div className="text-white flex flex-row space-x-4 font-dm text-2xl md:text-3xl lg:text-4xl">
+      <BiTrophy size={35} color="white" />
       <p className="w-28">{award}</p>
     </div>
   );
--- components/team.js
+++ components/team.js
@@ -1,12 +1,14 @@
 export default function Team({ src, name, role, pronouns }) {
   return (
-    <div className="flex flex-row space-x-3">
+    <div className="flex flex-row space-x-3 items-center">
       <div>
-        <img src={src} alt={name} className="rounded-full h-16 w-16" />
+        <img src={src} alt={name} className="rounded-full w-24 h-24" />
       </div>
       <div>
-        <p className="uppercase text-xl text-white font-ibm">{name}</p>
-        <p className="text-white text-lg font-dm">
+        <p className="uppercase text-xl text-white font-ibm lg:text-2xl">
+          {name}
+        </p>
+        <p className="text-white text-lg font-dm lg:text-xl">
           {role} <span className="text-blue-200">{pronouns}</span>
         </p>
       </div>
